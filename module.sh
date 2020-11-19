d77d80d27e9ed95d20b777d668dbd18fc11b0d0f
capitan comando <72467460+capitancomando@users.noreply.github.com>
2020-11-19 02:21:25 -0300

    Add files via upload

---
+++ module.sh
@@ -0,0 +1,114 @@
+#!bin/bash
+#by:Capitán Comando
+
+clear
+setterm -foreground red
+
+echo -e "###########################"
+
+echo -e "#-ESTE SCRIPT ESTA CREADO POR: CAPITÁN COMANDO."
+
+echo -e "###########################"
+
+echo -e "#-SOMOS NAHFER SOMOS LEGION SOMOS COMUNIDAD."
+
+echo -e "###########################"
+
+echo -e "#-SOCIETY PROGRAMATIONS HACKING."
+
+echo -e "###########################"
+
+setterm -foreground green
+
+echo -e "
+ ########################
+ ##                    ##
+ ## PYTHON SOLUTIONS   ##
+ ##        NO          ## 
+ ## MODULE MECHANIZE   ##
+ ##                    ##
+ ########################"
+            
+echo -e "\e[37m########################### \e[0m"
+
+echo -e "\e[32m INICIANDO SOLUCION\e[0m"
+echo ""
+sleep 2
+
+echo -e "###########################"
+
+echo -e "\e[32m LOANDING...\e[0m"
+echo ""
+sleep 2
+
+echo -e "###########################"
+
+echo -e "\e[32m [1] \e[0m"
+echo ""
+sleep 1
+
+echo -e "###########################"
+
+echo -e "\e[32m [2] \e[0m"
+echo ""
+sleep 1
+
+echo -e "###########################"
+
+echo -e "\e[32m [3] \e[0m"
+echo ""
+sleep 1
+
+echo -e "###########################"
+
+
+pip install --upgrade pip
+
+pip2 install mechanize 
+
+pip2 install mechanize --user
+
+pip2 install bs4 --user
+
+pip2 install whois --user
+
+pip install mechanize --user
+
+pip install mechanize
+
+pip install bs4 --user
+
+pip install whois --user
+
+pip3 install mechanize 
+
+pip3 install mechanize --user
+
+pip3 install bs4 --user
+
+pip3 install whois --user
+
+echo -e "###########################"
+
+echo -e "\e[32m TERMINANDO PROCESO \e[0m"
+echo ""
+sleep 1
+
+echo -e "###########################"
+
+echo -e "\e[32m LOANDING \e[0m"
+echo ""
+sleep 1
+
+echo -e "###########################"
+
+echo -e "PROCESO TERMINADO"
+
+echo -e "###########################"
+
+echo -e "ERROR NO MODULE MECHANIZE SOLUCIONADO"
+
+echo -e "###########################"
+
+
+exit
